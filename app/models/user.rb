eda9798115db64137410b98e7d2c4e19509ef2a6
Daniel Montoya <dsmontoyam@gmail.com>
2015-04-11 10:16:55 +0000

    Credits

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   has_many :charges_as_payer, foreign_key: 'payer_id', class_name: 'Charge'
   has_many :clients, through: :charges_as_receiver, source: :payer
   has_many :receivers, through: :charges_as_payer, source: :receiver
+  has_many :credits
 end
