e6c846ebef8767a7d08a515cbdf4fd3bea0741fa
Daniel Montoya <dsmontoyam@gmail.com>
2015-04-11 09:53:38 +0000

    Basic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+plans = Plan.create([
+  {weeks: 2},
+  {weeks: 4},
+  {weeks: 6}
+])
+
+users = User.create([
+  {name: "Daniel", phone: "+528332088924"},
+  {name: "Arturo", phone: "+525547318444"}
+])
+
+charges = Charge.create(amount: 100, receiver: User.first, payer: User.last, plan: Plan.first)
